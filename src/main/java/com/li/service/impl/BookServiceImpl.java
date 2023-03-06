16ff9625512f576d811d029d87d421ad032f5aff
lihengyi-art <3178633792@qq.com>
2023-03-06 14:12:06 +0800

    第一次

--- src/main/java/com/li/service/impl/BookServiceImpl.java
+++ src/main/java/com/li/service/impl/BookServiceImpl.java
@@ -3,4 +3,8 @@ package com.li.service.impl;
 import com.li.service.BookService;
 
 public class BookServiceImpl implements BookService {
+
+    public void speaker(){
+        System.out.println("Hello Git!");
+    }
 }
